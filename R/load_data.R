7780e1e28f73f0e22eca6f765520cc97f9e041e5
jincio <j.incio@pitt.edu>
2020-08-30 21:28:22 -0400

    Initial commit

---
+++ R/load_data.R
@@ -0,0 +1,76 @@
+#' load positive COVID cases
+#'
+#' Cargal la base de datos de casos confirmados desde la página de datos abiertos
+#'
+#' @return data_frame
+#' @export
+#'
+#' @examples
+da_positivos<-function (){
+  file="https://cloud.minsa.gob.pe/s/Y8w3wHsEdYQSZRp/download"
+  data=readr::read_csv(file, progress = TRUE,col_names = TRUE,
+                       locale(encoding = "latin1"),
+                       col_types = NULL)
+  data1 = dplyr::mutate(data,year = substr(FECHA_RESULTADO,1,4),
+                          month = substr(FECHA_RESULTADO,5,6),
+                          day = substr(FECHA_RESULTADO,7,8),
+                        fecha=as.Date(paste0(year,"-",month,"-",day)),
+                        EDAD_n = as.numeric(EDAD),
+                        semana = lubridate::epiweek(fecha))
+  return(data1)
+}
+#' load fallecidos Covid
+#'
+#'Carga la base de datos de Fallecidos desde la página de datos abiertos
+#'
+#' @return
+#' Data.frame con la información de "fallecidos" (datos abiertos). Agregando la variable fecha y semana.
+#' @export
+#'
+#' @examples
+#'
+da_fallecidos<-function (){
+  file="https://cloud.minsa.gob.pe/s/Md37cjXmjT9qYSa/download"
+  fallecidos=readr::read_csv(file, progress = TRUE,col_names = TRUE,
+                       locale(encoding = "latin1"),
+                       col_types = NULL)
+  fallecidos=mutate(fallecidos,year = substr(FECHA_FALLECIMIENTO,1,4),
+                    month = substr(FECHA_FALLECIMIENTO,5,6),
+                    day = substr(FECHA_FALLECIMIENTO,7,8),
+                    fecha = as.Date(paste0(year,"-",month,"-",day)),
+                    semana = lubridate::epiweek(fecha))
+  return(fallecidos)
+}
+
+#' da_sinadef
+#'
+#' @return
+#' @export
+#'
+#' @examples
+da_sinadef<-function (){
+  file="https://cloud.minsa.gob.pe/s/nqF2irNbFomCLaa/download"
+  data<- readr::read_csv2(file, progress = TRUE,col_names = FALSE,
+                    locale(encoding = "latin1"),
+                    col_types = NULL)
+  print("si lees esto es que el archivo bajo bien :)")
+  print("limpiando el archivo")
+  espacio_columna <- which(data[,1]=="Nº")[1]
+  col_names <- data[espacio_columna,]
+  colnames(data) <- col_names
+  colnames(data)[14] <-"Year"
+  inicio <- espacio_columna+1
+  print("Eliminamos informacion vacia")
+  data1 <- data[inicio:nrow(data),]
+  data1 <- data1[,colSums(is.na(data1))<nrow(data1)]
+  print("Creando variables standards")
+  data1 <- data1 %>% filter(`DEPARTAMENTO DOMICILIO` != "EXTRANJERO",
+                                           `MUERTE VIOLENTA` %in% c("SIN REGISTRO","NO SE CONOCE")) %>%
+    mutate(fecha = as.Date(FECHA), dia = lubridate::yday(fecha), mes = as.numeric(MES),
+           year = as.numeric(Year)) %>%
+    select(fecha,dia,mes,year,`DEPARTAMENTO DOMICILIO`,`PROVINCIA DOMICILIO`) %>%
+    group_by(fecha, `DEPARTAMENTO DOMICILIO`,`PROVINCIA DOMICILIO`) %>%
+    summarize(dia = mean(dia), mes = mean(mes), anho = mean(year),cont = n())
+
+  return(data1)
+}
---
+++ R/series_diarias.R
@@ -0,0 +1,431 @@
+#' fallecidos_diarios
+#'
+#' @param data Data frame creado con la función da_fallecidos()
+#' @param DEPARTAMENTO Departamento del Perú. Callao está listado como departamento
+#' @param mediamovil Integer para calcular o graficar la media móvil. Eje: 1,2,3.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fallecidos_diarios<-function(data,DEPARTAMENTO=NULL,mediamovil=NA){
+  if ("METODODX"%in%colnames(data)){
+    stop("La base de datos es incorrecta, use la función da_fallecidos()")
+  }
+
+  if(is.null(DEPARTAMENTO)){
+    titulo=paste0("Fallecidos por dia a nivel Nacional")
+    data=data
+
+    if (is.na(mediamovil))
+    {
+      f_dia_depa=data%>%
+        dplyr::select("fecha")%>%
+        group_by(fecha)%>%
+        summarize(count=n())
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = count)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos diarios",title = titulo)#+
+        #ggplot2::geom_line(size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    }
+    else{
+      k=mediamovil
+      f_dia_depa=data%>%
+        dplyr::select("fecha")%>%
+        group_by(fecha)%>%
+        summarize(count=n()) %>%
+        mutate(media.movil = zoo::rollmean(count,k=k, fill=NA,align = "right"))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = media.movil)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos diarios",title = titulo)+
+        ggplot2::geom_line(aes(x = fecha, y = count),size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)### Grafico aquí de barras con media movil?
+      #return(f_dia_depa)
+    }
+
+  } else{
+    DEPARTAMENTO=toupper(DEPARTAMENTO)
+    titulo=paste0("Fallecidos por dia en ",DEPARTAMENTO)
+    data=data
+    departamento_select=DEPARTAMENTO
+
+    if (is.na(mediamovil))
+    {
+      f_dia_depa=data%>%filter(DEPARTAMENTO==departamento_select)%>%
+        dplyr::select("DEPARTAMENTO","fecha")%>%
+        group_by(DEPARTAMENTO,fecha)%>%
+        summarize(count=n())
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = count)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos diarios",title = titulo)#+
+        #ggplot2::geom_line(size = 1.2,colour = "darkblue")
+
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    }
+    else{
+      k=mediamovil
+      f_dia_depa=data%>%filter(DEPARTAMENTO==departamento_select)%>%
+        dplyr::select("DEPARTAMENTO","fecha")%>%
+        group_by(DEPARTAMENTO,fecha)%>%
+        summarize(count=n()) %>% ungroup() %>%
+        mutate(media.movil = zoo::rollmean(count,k=k, fill=NA,align = "right"))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = media.movil)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos diarios",title = titulo)+
+        ggplot2::geom_line(aes(x = fecha, y = count),size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    }
+    return(resultado)
+  }
+  return(resultado)
+}
+
+#' casos_diarios
+#'
+#' @param data Data frame creado con la función da_positivos()
+#' @param DEPARTAMENTO Departamento del Perú. Callao está listado como departamento
+#' @param mediamovil Integer para calcular o graficar la media móvil. Eje: 1,2,3.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+casos_diarios<-function(data,DEPARTAMENTO=NULL,mediamovil=NA){
+  if (!"METODODX" %in% colnames(data)){
+    stop("La base de datos es incorrecta, use la función da_positivos()")
+  }
+  if(is.null(DEPARTAMENTO)){
+    titulo=paste0("Casos nuevos por dia a nivel Nacional")
+    data=data
+
+    if (is.na(mediamovil))
+    {
+      f_dia_depa=data%>%
+        dplyr::select("fecha")%>%
+        group_by(fecha)%>%
+        summarize(count=n())
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = count)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos diarios",title = titulo)#+
+        #ggplot2::geom_line(size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    }
+    else{
+      k=mediamovil
+      f_dia_depa=data%>%
+        dplyr::select("fecha")%>%
+        group_by(fecha)%>%
+        summarize(count=n()) %>%
+        mutate(media.movil = zoo::rollmean(count,k=k, fill=NA,align = "right"))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = media.movil)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos diarios",title = titulo)+
+        ggplot2::geom_line(aes(x = fecha, y = count),size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)### Grafico aquí de barras con media movil?
+      #return(f_dia_depa)
+    }
+
+  } else{
+    DEPARTAMENTO=toupper(DEPARTAMENTO)
+    titulo=paste0("Casos nuevos por dia en ",DEPARTAMENTO)
+    data=data
+    departamento_select=DEPARTAMENTO
+
+    if (is.na(mediamovil))
+    {
+      f_dia_depa=data%>%filter(DEPARTAMENTO==departamento_select)%>%
+        dplyr::select("DEPARTAMENTO","fecha")%>%
+        group_by(DEPARTAMENTO,fecha)%>%
+        summarize(count=n())
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = count)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos diarios",title = titulo)#+
+        #ggplot2::geom_line(size = 1.2,colour = "darkblue")
+
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    }
+    else{
+      k=mediamovil
+      f_dia_depa=data%>%filter(DEPARTAMENTO==departamento_select)%>%
+        dplyr::select("DEPARTAMENTO","fecha")%>%
+        group_by(DEPARTAMENTO,fecha)%>%
+        summarize(count=n()) %>% ungroup() %>%
+        mutate(media.movil = zoo::rollmean(count,k=k, fill=NA,align = "right"))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = media.movil)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos diarios",title = titulo)+
+        ggplot2::geom_line(aes(x = fecha, y = count),size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    }
+    return(resultado)
+  }
+  return(resultado)
+}
+
+#' sinadef_diarios
+#'
+#' @param data Data frame creado con la función da_sinadef()
+#' @param DEPARTAMENTO Departamento del Perú. Callao está listado como departamento
+#' @param mediamovil Integer para calcular o graficar la media móvil. Eje: 1,2,3.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sinadef_diarios<-function(data,DEPARTAMENTO=NULL,mediamovil=NA){
+  if ("METODODX"%in%colnames(data)){
+    stop("La base de datos es incorrecta, use la función da_sinadef()")
+  }
+
+  if(is.null(DEPARTAMENTO)){
+    titulo=paste0("Fallecidos por dia del 2020")
+    data=data
+
+    if (is.na(mediamovil))
+    {
+      f_dia_depa=data%>%
+        dplyr::select("fecha")%>% filter(fecha >"2020-01-01") %>%
+        group_by(fecha)%>%
+        summarize(count=n())%>%
+        ungroup()
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = count)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos diarios",title = titulo)#+
+      #ggplot2::geom_line(size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    }
+    else{
+      k=mediamovil
+      f_dia_depa=data%>%
+        dplyr::select("fecha")%>%
+        group_by(fecha)%>% filter(fecha >"2020-01-01") %>%
+        summarize(count=n()) %>%
+        mutate(media.movil = zoo::rollmean(count,k=k, fill=NA,align = "right"))%>%
+        ungroup()
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = media.movil)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos diarios",title = titulo)+
+        ggplot2::geom_line(aes(x = fecha, y = count),size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)### Grafico aquí de barras con media movil?
+      #return(f_dia_depa)
+    }
+
+  } else{
+    DEPARTAMENTO=toupper(DEPARTAMENTO)
+    titulo=paste0("Fallecidos Sinadef por dia en ",DEPARTAMENTO)
+    data=data
+    departamento_select=DEPARTAMENTO
+
+    if (is.na(mediamovil))
+    {
+      f_dia_depa=data%>%filter(DEPARTAMENTO==departamento_select)%>%
+        dplyr::select("DEPARTAMENTO","fecha")%>% filter(fecha >"2020-01-01") %>%
+        group_by(DEPARTAMENTO,fecha)%>%
+        summarize(count=n())%>%ungroup()
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = count)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos Sinadef diarios",title = titulo)#+
+      #ggplot2::geom_line(size = 1.2,colour = "darkblue")
+
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    }
+    else{
+      k=mediamovil
+      f_dia_depa=data%>%filter(DEPARTAMENTO==departamento_select)%>%
+        dplyr::select("DEPARTAMENTO","fecha")%>%  filter(fecha >"2020-01-01") %>%
+        group_by(DEPARTAMENTO,fecha)%>%
+        summarize(count=n()) %>% ungroup() %>%
+        mutate(media.movil = zoo::rollmean(count,k=k, fill=NA,align = "right"))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = media.movil)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos Sinadef diarios",title = titulo)+
+        ggplot2::geom_line(aes(x = fecha, y = count),size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    }
+    return(resultado)
+  }
+  return(resultado)
+}
+
+
+#' Fallecidos_ratio
+#'
+#' @param data Data frame creado con la función da_fallecidos()
+#' @param DEPARTAMENTO Departamento del Perú. Callao está listado como departamento
+#' @param mediamovil Integer para calcular o graficar la media móvil. Eje: 1,2,3.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fallecidos_crecimiento<-function(data,DEPARTAMENTO=NULL,mediamovil=NA){
+  if ("METODODX" %in% colnames(data)){
+    stop("La base de datos es incorrecta, use la función da_fallecidos()")
+  }
+  if(is.null(DEPARTAMENTO)){
+    titulo=paste0("Variacion % de Fallecidos por dia a nivel Nacional")
+    data=data
+    if (is.na(mediamovil))
+    {
+      f_dia_depa=data%>%
+        dplyr::select("fecha")%>%
+        group_by(fecha)%>%
+        summarize(count=n()) %>% mutate(growth = (count-lag(count))/lag(count))
+      #return(f_dia_depa)
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = growth)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos variacion",title = titulo)#+
+        #ggplot2::geom_line(size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)
+    }
+    else{
+      k=mediamovil
+      f_dia_depa=data%>%
+        dplyr::select("fecha")%>%
+        group_by(fecha)%>%
+        summarize(count=n()) %>%
+        mutate(media.movil = zoo::rollmean(count,k=k, fill=NA,align = "right"))%>%
+        mutate(growth = (media.movil-lag(media.movil))/lag(media.movil))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = growth)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos variacion",title = titulo)+
+        ggplot2::geom_line(aes(x = fecha, y = growth),size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)### Grafico aquí de barras con media movil?
+      #return(f_dia_depa)
+    }
+  } else{
+    DEPARTAMENTO=toupper(DEPARTAMENTO)
+    titulo=paste0("Variacion % de Fallecidos por dia en ",DEPARTAMENTO)
+    data=data
+    departamento_select=DEPARTAMENTO
+
+    if (is.na(mediamovil))
+    {
+      f_dia_depa=data%>%filter(DEPARTAMENTO==departamento_select)%>%
+        dplyr::select("DEPARTAMENTO","fecha")%>%
+        group_by(DEPARTAMENTO,fecha)%>%
+        summarize(count=n()) %>% mutate(growth = (count-lag(count))/lag(count))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = growth)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos variacion",title = titulo)#+
+        #ggplot2::geom_line(size = 1.2,colour = "darkblue")
+
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    }
+    else{
+      k=mediamovil
+      f_dia_depa=data%>%filter(DEPARTAMENTO==departamento_select)%>%
+        dplyr::select("DEPARTAMENTO","fecha")%>%
+        group_by(DEPARTAMENTO,fecha)%>%
+        summarize(count=n()) %>% ungroup() %>%
+        mutate(media.movil = zoo::rollmean(count,k=k, fill=NA,align = "right")) %>%
+        mutate(growth = (media.movil-lag(media.movil))/lag(media.movil))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = growth)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="fallecidos variacion",title = titulo)+
+        ggplot2::geom_line(aes(x = fecha, y = growth),size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)### Grafico aquí de barras con media movil?
+      #return(f_dia_depa)
+    }
+    return(resultado)
+  }
+  return(resultado)
+}
+
+#' Casos_crecimiento
+#'
+#' @param data Data frame creado con la función da_positivos()
+#' @param DEPARTAMENTO Departamento del Perú. Callao está listado como departamento
+#' @param mediamovil Integer para calcular o graficar la media móvil. Eje: 1,2,3.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+casos_crecimiento<-function(data,DEPARTAMENTO=NULL, mediamovil = NA){
+  if (!"METODODX" %in% colnames(data)){
+    stop("La base de datos es incorrecta, use la función da_positivos()")
+  }
+  if(is.null(DEPARTAMENTO)){
+    titulo=paste0("Variacion % de Casos por dia a nivel Nacional")
+    data=data
+
+    if (is.na(mediamovil))
+    {
+      f_dia_depa=data%>%
+        dplyr::select("fecha")%>%
+        group_by(fecha)%>%
+        summarize(count=n()) %>% mutate(growth = (count - lag(count))/lag(count))
+      #return(f_dia_depa)
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = growth)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="Casos variacion",title = titulo)#+
+        #ggplot2::geom_line(size = 1.2,colour = "darkblue")
+
+      resultado=list(f_dia_depa, grafico)
+    }
+    else{
+      k=mediamovil
+      f_dia_depa=data%>%
+        dplyr::select("fecha")%>%
+        group_by(fecha)%>%
+        summarize(count=n()) %>%
+        mutate(media.movil = zoo::rollmean(count,k=k, fill=NA,align = "right"))%>%
+        mutate(growth = (media.movil - lag(media.movil))/lag(media.movil))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = growth)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="Casos variacion",title = titulo)+
+        ggplot2::geom_line(aes(x = fecha, y = growth),size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)### Grafico aquí de barras con media movil?
+      #return(f_dia_depa)
+    }
+  } else{
+    DEPARTAMENTO=toupper(DEPARTAMENTO)
+    titulo=paste0("Variacion % de Casos por dia en",DEPARTAMENTO)
+    data=data
+    departamento_select=DEPARTAMENTO
+    if(is.na(mediamovil)){
+      f_dia_depa=data%>%filter(DEPARTAMENTO==departamento_select)%>%
+        dplyr::select("DEPARTAMENTO","fecha")%>%
+        group_by(DEPARTAMENTO,fecha)%>%
+        summarize(count=n()) %>% ungroup() %>% mutate(growth = (count-lag(count))/lag(count))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = growth)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="Casos Variacion",title = titulo)#+
+        #ggplot2::geom_line(size = 1.2,colour = "darkblue")
+
+      resultado=list(f_dia_depa, grafico)
+      #return(f_dia_depa)
+    } else {
+
+      k=mediamovil
+      f_dia_depa=data%>%filter(DEPARTAMENTO==departamento_select)%>%
+        dplyr::select("DEPARTAMENTO","fecha")%>%
+        group_by(DEPARTAMENTO,fecha)%>%
+        summarize(count=n()) %>% ungroup() %>%
+        mutate(media.movil = zoo::rollmean(count,k=k, fill=NA,align = "right")) %>%
+        mutate(growth = (media.movil - lag(media.movil))/lag(media.movil))
+      grafico <- f_dia_depa %>% ggplot2::ggplot(aes(x = fecha, y = growth)) +
+        ggplot2::geom_bar(stat="identity")+
+        labs(y="Casos Variacion",title = titulo)+
+        ggplot2::geom_line(aes(x = fecha, y = growth),size = 1.2,colour = "darkblue")
+      resultado=list(f_dia_depa, grafico)### Grafico aquí de barras con media movil?
+      #return(f_dia_depa)
+    }
+  }
+  return(resultado)
+}
+
+
+
