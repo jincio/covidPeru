71109e47040aeec9fef02a93d2c160d64bac14fa
jincio <j.incio@pitt.edu>
2020-10-27 23:47:09 -0400

    Change to data.table fread. Minsa changed the delimiter

--- R/load_data.R
+++ R/load_data.R
@@ -8,9 +8,7 @@
 #' @examples
 da_positivos<-function (){
   file="https://cloud.minsa.gob.pe/s/Y8w3wHsEdYQSZRp/download"
-  data=readr::read_csv(file, progress = TRUE,col_names = TRUE,
-                       readr::locale(encoding = "latin1"),
-                       col_types = NULL)
+  data=data.table::fread(file,encoding="Latin-1")
   data1 = dplyr::mutate(data,year = substr(FECHA_RESULTADO,1,4),
                           month = substr(FECHA_RESULTADO,5,6),
                           day = substr(FECHA_RESULTADO,7,8),
@@ -31,14 +29,12 @@ da_positivos<-function (){
 #'
 da_fallecidos<-function (){
   file="https://cloud.minsa.gob.pe/s/Md37cjXmjT9qYSa/download"
-  fallecidos=readr::read_csv(file, progress = TRUE,col_names = TRUE,
-                       readr::locale(encoding = "latin1"),
-                       col_types = NULL)
+  data=data.table::fread(file,encoding="Latin-1")
   fallecidos=dplyr::mutate(fallecidos,year = substr(FECHA_FALLECIMIENTO,1,4),
-                    month = substr(FECHA_FALLECIMIENTO,5,6),
-                    day = substr(FECHA_FALLECIMIENTO,7,8),
-                    fecha = as.Date(paste0(year,"-",month,"-",day)),
-                    semana = lubridate::epiweek(fecha))
+                           month = substr(FECHA_FALLECIMIENTO,5,6),
+                           day = substr(FECHA_FALLECIMIENTO,7,8),
+                           fecha = as.Date(paste0(year,"-",month,"-",day)),
+                           semana = lubridate::epiweek(fecha))
   return(fallecidos)
 }
 
@@ -50,19 +46,17 @@ da_fallecidos<-function (){
 #' @examples
 da_sinadef<-function (){
   file="https://cloud.minsa.gob.pe/s/nqF2irNbFomCLaa/download"
-  data<- readr::read_csv2(file, progress = TRUE,col_names = FALSE,
-                          readr::locale(encoding = "latin1"),
-                          col_types = NULL)
+  data.table::fread(file,encoding="Latin-1")
   cat("si lees esto es que el archivo bajo bien :)")
   cat("...limpiando el archivo")
   espacio_columna <- which(data[,1]=="Nº")[1]
   col_names <- data[espacio_columna,]
-  colnames(data) <- col_names
+  colnames(data) <- data.frame(t(col_names))[,1]
   colnames(data)[14] <-"Year"
   inicio <- espacio_columna+1
   cat("...Eliminamos informacion vacia")
   data1 <- data[inicio:nrow(data),]
-  data1 <- data1[,colSums(is.na(data1))<nrow(data1)]
+  data1 <- data1 %>% dplyr::select_if(~sum(!is.na(.)) > 0)
   cat("...Creando variables standards")
   data1 <- data1 %>% dplyr::filter(`DEPARTAMENTO DOMICILIO` != "EXTRANJERO",
                             `MUERTE VIOLENTA` %in% c("SIN REGISTRO","NO SE CONOCE")) %>%
